147cb007c7b74e9720aa399b65813adb9d35f046
Yaroslav Rybalka <60896665+markus2812@users.noreply.github.com>
2023-09-26 16:49:46 +0300

    Created a simple display of information about the kitchen

--- kitchen/views.py
+++ kitchen/views.py
@@ -7,10 +7,10 @@ def index(request: HttpRequest) -> HttpResponse:
     num_of_cook = Cook.objects.count()
     num_of_dish = Dish.objects.count()
     num_of_dish_type = DishType.objects.count()
-    return HttpResponse(f"<h1>Hello Mate!</h1>"
-                        f"<ul>"
-                        f"<li>Num of cook: {num_of_cook}</li>"
-                        f"<li>Num of dish: {num_of_dish}</li>"
-                        f"<li>Num of dish-type: {num_of_dish_type}</li>"
-                        f"</ul>")
+    context = {
+        "num_of_cook": num_of_cook,
+        "num_of_dish": num_of_dish,
+        "num_of_dish_type": num_of_dish_type
+    }
+    return render(request, "kitchen/index.html" ,context=context)
 
