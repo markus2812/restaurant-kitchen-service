7b72100d9428ea97b1fcb1de9bfa11dccd895d4b
Yaroslav Rybalka <60896665+markus2812@users.noreply.github.com>
2023-09-26 14:13:09 +0300

    Add new models and fill the table with information

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,37 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ("name", )
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
+
+    class Meta:
+        ordering = ("name", )
+
+    def __str__(self):
+        return f"{self.name}{self.price}"
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        ordering = ("username", )
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
