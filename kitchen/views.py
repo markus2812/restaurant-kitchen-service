52baf0b6781e7493ca55ffffc92c9182e6545bad
Yaroslav Rybalka <60896665+markus2812@users.noreply.github.com>
2023-09-26 16:41:36 +0300

    Create a kitchen directory in templates

--- kitchen/views.py
+++ kitchen/views.py
@@ -7,7 +7,7 @@ def index(request: HttpRequest) -> HttpResponse:
     num_of_cook = Cook.objects.count()
     num_of_dish = Dish.objects.count()
     num_of_dish_type = DishType.objects.count()
-    return HttpResponse(f"<h1>Hello Katya!</h1>"
+    return HttpResponse(f"<h1>Hello Mate!</h1>"
                         f"<ul>"
                         f"<li>Num of cook: {num_of_cook}</li>"
                         f"<li>Num of dish: {num_of_dish}</li>"
