f5255aac8c695641f50a28f534b8b44cbb1033ca
Yaroslav Rybalka <60896665+markus2812@users.noreply.github.com>
2023-09-28 14:24:39 +0300

    Add dish list and dish type list

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from kitchen.views import index
+from kitchen.views import index, DishTypeListView, DishListView
+
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
+    path("dishes/", DishListView.as_view(), name="dish-list")
 ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
+
 from .models import Cook, Dish, DishType
 
 
@@ -14,3 +16,12 @@ def index(request: HttpRequest) -> HttpResponse:
     }
     return render(request, "kitchen/index.html", context=context)
 
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    template_name = "kitchen/dish_type_list.html"
+    context_object_name = "dish_type_list"
+
+
+class DishListView(generic.ListView):
+    model = Dish
